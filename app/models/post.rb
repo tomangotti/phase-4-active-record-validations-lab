3d7183b171d1becf11265dd482a71e6abb65859e
thomas <tom.angotti11@gmail.com>
2023-02-22 16:09:38 -0700

    commit tree 4a023c24d4a08f210fe2536b9daf26374b288b79

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { :minimum => 250 }
+    validates :summary, length: { :maximum => 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :clickbait?
+
+    CLICKBAIT_PHRASE = [
+        /Won't Believe/i,
+        /Secrete/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PHRASE.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait")
+        end
+
+    end
+
 end
